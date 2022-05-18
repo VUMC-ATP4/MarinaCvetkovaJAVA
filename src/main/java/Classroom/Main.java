20fbef6c20fd6fd6301b87fa35a428419a6f82ed
marina-cvetkova <105214216+marina-cvetkova@users.noreply.github.com>
2022-05-18 18:26:44 +0300

    md

--- src/main/java/Classroom/Main.java
+++ src/main/java/Classroom/Main.java
@@ -1,70 +1,98 @@
-package Classroom;
+package classroom;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println( " Hello Marina pirmā lekcija");
-        // Integer  Vesels skaitlis
+        System.out.println("Mācāmies mainīgos");
+
+        //EN:Integer - LV:Vesels skaitlis
 
         int x = 5;
-        int y =10;
+        int y = 10;
         int q = -10;
-double  lowestTempretore = 10.2;
-float highestTempretore = 32F;
+
+        double lowestTemperature = -20.4;
+        float highestTemperature = 40.65512312F;
+        System.out.println("Augstaka temp ir " + highestTemperature + "zemaka temp ir " + lowestTemperature);
 
 
-
+        System.out.println("X ir vienāds ar:");
         System.out.println(x);
+        System.out.println("Y ir vienāds ar:");
         System.out.println(y);
-        System.out.println(x+y);
+        System.out.println("x+y ir vienads ar:");
+        System.out.println(x + y);
+        System.out.println(x + y);
 
-        ;
-
-
-
-// [mainīgā tips] [Mainīga nosaukums] = [Vertiba]
-// Vārdi un teikumi JAVA valoda tiek saglabāti mainīgā tipā "String"
-        String dogName= "Nasa";
-
-//        Veseli skaitļi tiek ar mainīgo tipu "int
-        int dogAge = 10;
-//Daiļskaitļi tiek definēti armainīgā tipu " double vai float"
-
-        double dogWeight = 30.5;
+        //[Mainīga tips] [Mainīgā nosaukums] = [Vērtība];
+        //Vārdi un teikumi JAVA valodā tiek saglabāti mainīgā tipā 'String'
+        String dogName = "Reksis";
+        //Veseli skaitļi tiek definēti ar mainīgā tipu 'int'
+        int dogAge = 5;
+        //Daļskaitļi tiek definēti ar mainīgā tipu 'double' vai 'float'
+        double dogWeigth = 15.5;
         String dogBreed = "VAS";
-//        ja (true) nē (false) jautajumi tiek definēts ar mainīgo "boolean"
-        boolean isDogHungrey = true;
+        //Jā(true) nē(false) jautājums tiek definēts ar mainīgā tipu 'boolean'
+        boolean isDogHungry = false;
+        boolean isDogVaccinated = true;
+        //vienu simbolu definē ar mainīgā tipu 'char'
+        char currency = '€';
+        int dogPrice = 100;
+        char dogGender = 'M';
 
-// vienu simbolu definē ar mainīgo tipu "char"
+        System.out.println("Suņa Vārds:" + dogName);
+        System.out.println("Suņa Vecums: " + dogAge);
+        System.out.println("Suņa Svars: " + dogWeigth);
+        System.out.println("Suņa Škirne: " + dogBreed);
+        System.out.println("Vai suns ir izsalcis? " + isDogHungry);
+        System.out.println("Vai suns ir vakcinēts? " + isDogVaccinated);
+        System.out.println("Suņa dzimums ir: " + dogGender);
+        System.out.println("Suņa kucēni maksā: " + dogPrice + currency);
 
-       char Doggender = 'M';
-
-       char currency ='$';
-
-
-        System.out.println("Suņa vārds:" + "dogName");
-        System.out.println( "Suņa svars:"+  "dogWeight");
-        System.out.println("Suņa šķirne:"+  "dogBreed");
-        System.out.println("Vai suns ir izsalcis?:" + "isDogHungrey");
-        System.out.println( "Suņa vecums ir + Doggender");
 
         byte age = 10;
-        int deapesSeeLevel = 111100000;
-        int HigestIntNumber = 2146732;
-        System.out.println( age);
-        System.out.println( deapesSeeLevel);
+        short deepestSeaLevel = 11000;
+        int highestIntNumber = 2147483647;
+        long USACountryDebt = 21474836471232L;
+        System.out.println(age);
+        System.out.println(deepestSeaLevel);
+        System.out.println(highestIntNumber);
+        System.out.println(USACountryDebt);
 
-        int summa;
-        int pirmaisSkaitlis = 18;
-        int otraisSklaitlis =8;
-        summa = pirmaisSkaitlis + otraisSklaitlis;
-        System.out.println(pirmaisSkaitlis + "+" + otraisSklaitlis + "=" + summa);
+        int starpiba;
+        int pirmaisSkaitlis = 20;
+        int otraisSkaitlis = 10;
 
 
-int dalijums = pirmaisSkaitlis / otraisSklaitlis;
-int reizinajums = pirmaisSkaitlis * otraisSklaitlis;
-int atlikums = pirmaisSkaitlis % otraisSklaitlis;
-        String starpiba = null;
-        System.out.println("starpiba" + "starpiba");
+        starpiba = pirmaisSkaitlis - otraisSkaitlis;
+        int summa = pirmaisSkaitlis + otraisSkaitlis;
+        int dalijums = pirmaisSkaitlis / otraisSkaitlis;
+        int reizinajums = pirmaisSkaitlis * otraisSkaitlis;
+        int atlikums = 10 % 3;
+
+        double a = 3.5;
+        int b = 5;
+        System.out.println((double) b);
+        System.out.println((int) a);
+
+
+        int c = 7;
+
+        System.out.println("########"+b/a);
+
+
+
+        System.out.println("Ja atlikums ir 0, tad para skaitlis" + b % 2);
+
+        System.out.println("starpiba" + starpiba);
+        System.out.println("summa" + summa);
+        System.out.println("dalijums" + dalijums);
+        System.out.println("reizinajums" + reizinajums);
+        System.out.println("atlikums" + atlikums);
+
 
     }
+
+
 }
--- src/main/java/homepage/FirstJClass.java
+++ src/main/java/homepage/FirstJClass.java
@@ -3,5 +3,85 @@ package homepage;
 public class FirstJClass {
     public static void main(String[] args) {
         System.out.println(" This is homework");
+        String countryName= "Spain";
+        int popilation= 46030109;
+        String capitalCity= "Madrid";
+        String officialLanguage= "Spanish";
+        String inThe= "European Union";
+        char currency ='€';
+        System.out.println("Country Name:" + "Spain");
+        System.out.println("Popilation:" + "46030109");
+        System.out.println("CapitalCity:" + "Madrid");
+        System.out.println("OfficialLanguage:" + "Spanish");
+        System.out.println("In The:" + "European Union");
+        System.out.println("Currency:" + "€");
+
+        String students = "People in the class";
+        int summa;
+        int girl = 23;
+        int boys = 11;
+        int teacher = 1;
+        int schoolCost = 13;
+
+        System.out.println("People in the class");
+        System.out.println(girl);
+        System.out.println(boys);
+        System.out.println(teacher);
+        summa = girl+ boys+ teacher;
+        System.out.println(girl + "+" + boys+ "+" + teacher + "=" + summa);
+        System.out.println("How much  does school cost per day?: " + schoolCost + currency);
+
+        int starpiba;
+        int pirmaisSkaitlis = 13;
+        int otraisSkaitlis = 10;
+        starpiba = pirmaisSkaitlis - otraisSkaitlis;
+        System.out.println(pirmaisSkaitlis + "-" +  otraisSkaitlis + "= "+starpiba);
+
+        int x = 5;
+        int y = 2;
+        int z = x - y;
+        System.out.println(x + "-" +  y + "="+ z);
+
+        int k = 20;
+        int l = 10;
+        int p = 10;
+        System.out.println(k + "-" +  l + "="+ p);
+
+
+        int n = 30;
+        int f = 12;
+        System.out.println(x + y);
+
+        int h = 5;
+        int d = 2;
+        int r = 7;
+        System.out.println(h + "+" +  d + "="+ r);
+
+        double g = 5.6;
+        double w = 3.3;
+        System.out.println(g + w);
+
+        int dalijums = pirmaisSkaitlis / otraisSkaitlis;
+        System.out.println(pirmaisSkaitlis + "/" +  otraisSkaitlis + "= "+dalijums);
+        System.out.println(x / y);
+        System.out.println(g / w);
+
+        int reizinajums = pirmaisSkaitlis * otraisSkaitlis;
+        System.out.println(pirmaisSkaitlis + "*" +  otraisSkaitlis + "= "+reizinajums);
+        System.out.println(x * y);
+
+        int atlikums = pirmaisSkaitlis * otraisSkaitlis;
+        System.out.println(pirmaisSkaitlis + "%" +  otraisSkaitlis + "= "+atlikums);
+        System.out.println(h + "%" +  d + "="+ r);
+
+
+
+
+
+
+
+
+
+
     }
 }
